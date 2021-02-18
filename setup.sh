0af1a7dbec704b30df8b8ea4d2455ec0fe8cdcb8
pêle-mêle <kumo.git@mailnull.com>
2021-02-19 00:26:01 +0100

    begin configmap

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-minikube addons list |;
-https://stackoverflow.com/questions/57821066/how-to-update-minikube-latest-version
+#minikube addons list |;
+#https://stackoverflow.com/questions/57821066/how-to-update-minikube-latest-version
 
 eval $(minikube docker-env)
 
-docker build -t nginx-image nginx
+docker build -t nginx-image:1.0 nginx
